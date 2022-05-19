1a88a3fa0e312aef5ece3d1dd0c2f53e0269a03c
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-05-19 20:20:47 +0900

    MVC 패턴 - 적용

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.basic.HttpServlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);// 컨트롤러에서 뷰로 이동될 때 사용
+        dispatcher.forward(request, response); // 서블릿에서 jsp 호출 가능
+
+    }
+}
